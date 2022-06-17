2b6b638cd02ab2a418b3bdf822ceff40668a8e3e
arjun149 <69946331+arjun149@users.noreply.github.com>
2022-06-17 17:35:51 -0400

    Update spmodel_test.py

--- tests/spmodel_test.py
+++ tests/spmodel_test.py
@@ -1,18 +1,16 @@
 import unittest
 
 import numpy as np
-
 from HCRSimPY.models import SinglePopModel
 
 
 class SinglePopModelTests(unittest.TestCase):
-
-
+    
     def testAlpha(self):
         sp = SinglePopModel(lambda t: 100.0)
-        for t in np.arange(0,100,0.1):
-            aval=sp.alpha0(t)
-            self.assertGreaterEqual(aval,0.0, msg="Test alpha function")
+        for t in np.arange(0, 100, 0.1):
+            aval = sp.alpha0(t)
+            self.assertGreaterEqual(aval, 0.0, msg="Test alpha function")
 
 
 if __name__ == '__main__':
