d920e9ac8d8b639c740d761eb4ab663b33090c06
Kevin Hannay <13168556+khannay@users.noreply.github.com>
2022-11-29 13:23:13 -0600

    spaces

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements=["scipy>=1.3"]
+requirements = ["scipy>=1.3"]
 setup(
     name="HCRSimPY",
     version="1.0.4",
