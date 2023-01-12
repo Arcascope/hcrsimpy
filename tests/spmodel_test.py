fcfc8d19d597b913f43ba62f7b4277ea76eebeeb
Kevin Hannay <13168556+khannay@users.noreply.github.com>
2023-01-12 07:43:10 -0600

    update requirements

--- tests/spmodel_test.py
+++ tests/spmodel_test.py
@@ -5,12 +5,15 @@ from hcrsimpy.models import SinglePopModel
 
 
 class SinglePopModelTests(unittest.TestCase):
-    
-    def testAlpha(self):
-        sp = SinglePopModel(lambda t: 100.0)
-        for t in np.arange(0, 100, 0.1):
-            aval = sp.alpha0(t)
-            self.assertGreaterEqual(aval, 0.0, msg="Test alpha function")
+            
+    def test_darkness_integration(self):
+        model = SinglePopModel()
+        ts = np.arange(0,24*30, 0.10)
+        light_est = np.zeros(len(ts))
+        sol = model.integrate_model(ts, light_est, np.array([1.0, 0.0,0.0]))
+        self.assertAlmostEquals(187.05691072, 
+                                sol[1,-1],
+                                msg = "Darkness integration not running with the correct period")
 
 
 if __name__ == '__main__':
